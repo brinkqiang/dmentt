77a1fb7ec48151d9d5dcfa191e8e17e2c89cf962
brinkqiang <brink.qiang@gmail.com>
2024-08-16 14:16:46 +0800

    add

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ rmdir /S /Q build
 mkdir build
 pushd build
 
-cmake -A x64 -DCMAKE_BUILD_TYPE=relwithdebinfo ..
+cmake -A x64 -DCMAKE_BUILD_TYPE=relwithdebinfo -DENTT_BUILD_TESTING=ON ..
 cmake --build . --config relwithdebinfo -- /m:%NUMBER_OF_PROCESSORS%
 popd
 
