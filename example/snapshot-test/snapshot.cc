3e16863795b37659184087e72c6a0c15378a00ea
brinkqiang <brink.qiang@gmail.com>
2025-04-28 08:24:33 +0800

    add

--- example/snapshot-test/snapshot.cc
+++ example/snapshot-test/snapshot.cc
@@ -8,8 +8,8 @@ int main() {
     using traits_type = entt::entt_traits<entt::entity>;
 
     entt::registry registry;
-    const entt::basic_snapshot snapshot{registry};
-    const auto &storage = registry.storage<entt::entity>();
+    entt::basic_snapshot snapshot{registry};
+    auto &storage = registry.storage<entt::entity>();
 
     std::vector<entt::any> data{};
     auto archive = [&data](auto &&elem) { data.emplace_back(std::forward<decltype(elem)>(elem)); };
